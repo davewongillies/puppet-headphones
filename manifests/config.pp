3fedb945a0eecc98db46c0f7100c44db5400afb2
lbennett <lbennett@mimecast.com>
2012-09-30 18:52:22 +0100

    config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,20 @@
 class headphones::config {
 
     $headphones_data = "/usr/local/headphones-data"
+    $headphones_port = extlookup("headphones_port")
 
     file { "/usr/local/headphones-data":
         ensure => directory,
         owner => 'headphones',
         group => 'headphones',
         recurse => 'true'
+    }
+
+    file { "/usr/local/headphones-data/config.ini":
+        content => template('headphones/config.ini.erb'),
+        owner => 'headphones',
+        group => 'headphones',
+        mode => '0644',
+        require => File['/usr/local/headphones-data']
     }
 }
